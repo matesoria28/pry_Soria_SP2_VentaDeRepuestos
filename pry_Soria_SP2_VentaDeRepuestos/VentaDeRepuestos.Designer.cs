d61eabd532ef798933981c2c2bdbb37949131fb2
Alumno <Alumno@PC4>
2025-09-22 14:29:43 -0300

    Agregar archivos de proyecto.

---
+++ pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace pry_Soria_SP2_VentaDeRepuestos
+{
+    partial class VentaDeRepuestos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblMarca = new Label();
+            lblOrigen = new Label();
+            lblNumero = new Label();
+            lblDescripcion = new Label();
+            lblmarca2 = new Label();
+            lblOrigen2 = new Label();
+            rdbPeugeot = new RadioButton();
+            rdbFiat = new RadioButton();
+            rdbRenault = new RadioButton();
+            txtNumero = new TextBox();
+            txtDescripcion = new TextBox();
+            btnIngresarRepuestos = new Button();
+            cmbMarca2 = new ComboBox();
+            cmbOrigen2 = new ComboBox();
+            listBox1 = new ListBox();
+            rdbNacional = new RadioButton();
+            rdbImportado = new RadioButton();
+            SuspendLayout();
+            // 
+            // lblMarca
+            // 
+            lblMarca.AutoSize = true;
+            lblMarca.Location = new Point(22, 37);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(43, 15);
+            lblMarca.TabIndex = 3;
+            lblMarca.Text = "Marca:";
+            // 
+            // lblOrigen
+            // 
+            lblOrigen.AutoSize = true;
+            lblOrigen.Location = new Point(22, 84);
+            lblOrigen.Name = "lblOrigen";
+            lblOrigen.Size = new Size(46, 15);
+            lblOrigen.TabIndex = 4;
+            lblOrigen.Text = "Origen:";
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(22, 121);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(54, 15);
+            lblNumero.TabIndex = 5;
+            lblNumero.Text = "Numero:";
+            // 
+            // lblDescripcion
+            // 
+            lblDescripcion.AutoSize = true;
+            lblDescripcion.Location = new Point(22, 160);
+            lblDescripcion.Name = "lblDescripcion";
+            lblDescripcion.Size = new Size(72, 15);
+            lblDescripcion.TabIndex = 6;
+            lblDescripcion.Text = "Descripcion:";
+            // 
+            // lblmarca2
+            // 
+            lblmarca2.AutoSize = true;
+            lblmarca2.Location = new Point(22, 272);
+            lblmarca2.Name = "lblmarca2";
+            lblmarca2.Size = new Size(43, 15);
+            lblmarca2.TabIndex = 7;
+            lblmarca2.Text = "Marca:";
+            // 
+            // lblOrigen2
+            // 
+            lblOrigen2.AutoSize = true;
+            lblOrigen2.Location = new Point(22, 314);
+            lblOrigen2.Name = "lblOrigen2";
+            lblOrigen2.Size = new Size(46, 15);
+            lblOrigen2.TabIndex = 8;
+            lblOrigen2.Text = "Origen:";
+            // 
+            // rdbPeugeot
+            // 
+            rdbPeugeot.AutoSize = true;
+            rdbPeugeot.Location = new Point(112, 35);
+            rdbPeugeot.Name = "rdbPeugeot";
+            rdbPeugeot.Size = new Size(69, 19);
+            rdbPeugeot.TabIndex = 9;
+            rdbPeugeot.TabStop = true;
+            rdbPeugeot.Text = "Peugeot";
+            rdbPeugeot.UseVisualStyleBackColor = true;
+            // 
+            // rdbFiat
+            // 
+            rdbFiat.AutoSize = true;
+            rdbFiat.Location = new Point(206, 35);
+            rdbFiat.Name = "rdbFiat";
+            rdbFiat.Size = new Size(44, 19);
+            rdbFiat.TabIndex = 10;
+            rdbFiat.TabStop = true;
+            rdbFiat.Text = "Fiat";
+            rdbFiat.UseVisualStyleBackColor = true;
+            // 
+            // rdbRenault
+            // 
+            rdbRenault.AutoSize = true;
+            rdbRenault.Location = new Point(274, 35);
+            rdbRenault.Name = "rdbRenault";
+            rdbRenault.Size = new Size(65, 19);
+            rdbRenault.TabIndex = 11;
+            rdbRenault.TabStop = true;
+            rdbRenault.Text = "Renault";
+            rdbRenault.UseVisualStyleBackColor = true;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(108, 113);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(231, 23);
+            txtNumero.TabIndex = 13;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(108, 152);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(231, 23);
+            txtDescripcion.TabIndex = 14;
+            // 
+            // btnIngresarRepuestos
+            // 
+            btnIngresarRepuestos.Location = new Point(139, 202);
+            btnIngresarRepuestos.Name = "btnIngresarRepuestos";
+            btnIngresarRepuestos.Size = new Size(167, 23);
+            btnIngresarRepuestos.TabIndex = 15;
+            btnIngresarRepuestos.Text = "Ingresar Repuestos";
+            btnIngresarRepuestos.UseVisualStyleBackColor = true;
+            // 
+            // cmbMarca2
+            // 
+            cmbMarca2.FormattingEnabled = true;
+            cmbMarca2.Location = new Point(109, 264);
+            cmbMarca2.Name = "cmbMarca2";
+            cmbMarca2.Size = new Size(121, 23);
+            cmbMarca2.TabIndex = 16;
+            // 
+            // cmbOrigen2
+            // 
+            cmbOrigen2.FormattingEnabled = true;
+            cmbOrigen2.Location = new Point(108, 306);
+            cmbOrigen2.Name = "cmbOrigen2";
+            cmbOrigen2.Size = new Size(121, 23);
+            cmbOrigen2.TabIndex = 17;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(22, 348);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(317, 109);
+            listBox1.TabIndex = 20;
+            // 
+            // rdbNacional
+            // 
+            rdbNacional.AutoSize = true;
+            rdbNacional.Location = new Point(112, 80);
+            rdbNacional.Name = "rdbNacional";
+            rdbNacional.Size = new Size(72, 19);
+            rdbNacional.TabIndex = 21;
+            rdbNacional.TabStop = true;
+            rdbNacional.Text = "Nacional";
+            rdbNacional.UseVisualStyleBackColor = true;
+            // 
+            // rdbImportado
+            // 
+            rdbImportado.AutoSize = true;
+            rdbImportado.Location = new Point(206, 80);
+            rdbImportado.Name = "rdbImportado";
+            rdbImportado.Size = new Size(81, 19);
+            rdbImportado.TabIndex = 22;
+            rdbImportado.TabStop = true;
+            rdbImportado.Text = "Importado";
+            rdbImportado.UseVisualStyleBackColor = true;
+            // 
+            // VentaDeRepuestos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(386, 469);
+            Controls.Add(rdbImportado);
+            Controls.Add(rdbNacional);
+            Controls.Add(listBox1);
+            Controls.Add(cmbOrigen2);
+            Controls.Add(cmbMarca2);
+            Controls.Add(btnIngresarRepuestos);
+            Controls.Add(txtDescripcion);
+            Controls.Add(txtNumero);
+            Controls.Add(rdbRenault);
+            Controls.Add(rdbFiat);
+            Controls.Add(rdbPeugeot);
+            Controls.Add(lblOrigen2);
+            Controls.Add(lblmarca2);
+            Controls.Add(lblDescripcion);
+            Controls.Add(lblNumero);
+            Controls.Add(lblOrigen);
+            Controls.Add(lblMarca);
+            Name = "VentaDeRepuestos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Venta de Repuestos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblMarca;
+        private Label lblOrigen;
+        private Label lblNumero;
+        private Label lblDescripcion;
+        private Label lblmarca2;
+        private Label lblOrigen2;
+        private RadioButton rdbPeugeot;
+        private RadioButton rdbFiat;
+        private RadioButton rdbRenault;
+        private TextBox txtNumero;
+        private TextBox txtDescripcion;
+        private Button btnIngresarRepuestos;
+        private ComboBox cmbMarca2;
+        private ComboBox cmbOrigen2;
+        private ListBox listBox1;
+        private RadioButton rdbNacional;
+        private RadioButton rdbImportado;
+    }
+}
---
+++ pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.cs
@@ -0,0 +1,10 @@
+namespace pry_Soria_SP2_VentaDeRepuestos
+{
+    public partial class VentaDeRepuestos : Form
+    {
+        public VentaDeRepuestos()
+        {
+            InitializeComponent();
+        }
+    }
+}

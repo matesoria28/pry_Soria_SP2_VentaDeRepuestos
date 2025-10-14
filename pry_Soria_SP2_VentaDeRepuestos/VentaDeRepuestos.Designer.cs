b707a84c08cf895dd6ab022c396b054b6e44ec82
mateo soria <matesoria28@gmail.com>
2025-10-14 14:19:43 -0300

    14/10/25 se agrego la matriz pero no lo cargue

--- pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.Designer.cs
+++ pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.Designer.cs
@@ -32,25 +32,36 @@
             lblOrigen = new Label();
             lblNumero = new Label();
             lblDescripcion = new Label();
-            lblmarca2 = new Label();
+            Datos = new TabControl();
+            tbcIngreso = new TabPage();
+            ltbDescripcion = new ListBox();
+            mskPrecio = new MaskedTextBox();
+            cmbNumero = new ComboBox();
+            cmbMarca = new ComboBox();
+            btnRegistrar = new Button();
+            cmbOrigen = new ComboBox();
+            lblPrecio = new Label();
+            tbcDatos = new TabPage();
             lblOrigen2 = new Label();
-            rdbPeugeot = new RadioButton();
-            rdbFiat = new RadioButton();
-            rdbRenault = new RadioButton();
-            txtNumero = new TextBox();
-            txtDescripcion = new TextBox();
-            btnIngresarRepuestos = new Button();
-            cmbMarca2 = new ComboBox();
-            cmbOrigen2 = new ComboBox();
-            listBox1 = new ListBox();
-            rdbNacional = new RadioButton();
-            rdbImportado = new RadioButton();
+            lblMarca2 = new Label();
+            cmbOrigenFiltro = new ComboBox();
+            cboMarcaFiltro = new ComboBox();
+            dgvDatos = new DataGridView();
+            colMarca = new DataGridViewTextBoxColumn();
+            colOrigen = new DataGridViewTextBoxColumn();
+            colNumero = new DataGridViewTextBoxColumn();
+            colPrecio = new DataGridViewTextBoxColumn();
+            colDescripcion = new DataGridViewTextBoxColumn();
+            Datos.SuspendLayout();
+            tbcIngreso.SuspendLayout();
+            tbcDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
             SuspendLayout();
             // 
             // lblMarca
             // 
             lblMarca.AutoSize = true;
-            lblMarca.Location = new Point(22, 37);
+            lblMarca.Location = new Point(6, 16);
             lblMarca.Name = "lblMarca";
             lblMarca.Size = new Size(43, 15);
             lblMarca.TabIndex = 3;
@@ -59,7 +70,7 @@
             // lblOrigen
             // 
             lblOrigen.AutoSize = true;
-            lblOrigen.Location = new Point(22, 84);
+            lblOrigen.Location = new Point(230, 16);
             lblOrigen.Name = "lblOrigen";
             lblOrigen.Size = new Size(46, 15);
             lblOrigen.TabIndex = 4;
@@ -68,7 +79,7 @@
             // lblNumero
             // 
             lblNumero.AutoSize = true;
-            lblNumero.Location = new Point(22, 121);
+            lblNumero.Location = new Point(6, 55);
             lblNumero.Name = "lblNumero";
             lblNumero.Size = new Size(54, 15);
             lblNumero.TabIndex = 5;
@@ -77,160 +88,206 @@
             // lblDescripcion
             // 
             lblDescripcion.AutoSize = true;
-            lblDescripcion.Location = new Point(22, 160);
+            lblDescripcion.Location = new Point(6, 96);
             lblDescripcion.Name = "lblDescripcion";
             lblDescripcion.Size = new Size(72, 15);
             lblDescripcion.TabIndex = 6;
             lblDescripcion.Text = "Descripcion:";
             // 
-            // lblmarca2
+            // Datos
             // 
-            lblmarca2.AutoSize = true;
-            lblmarca2.Location = new Point(22, 272);
-            lblmarca2.Name = "lblmarca2";
-            lblmarca2.Size = new Size(43, 15);
-            lblmarca2.TabIndex = 7;
-            lblmarca2.Text = "Marca:";
+            Datos.Controls.Add(tbcIngreso);
+            Datos.Controls.Add(tbcDatos);
+            Datos.Location = new Point(12, 12);
+            Datos.Name = "Datos";
+            Datos.SelectedIndex = 0;
+            Datos.Size = new Size(478, 383);
+            Datos.TabIndex = 21;
+            // 
+            // tbcIngreso
+            // 
+            tbcIngreso.Controls.Add(ltbDescripcion);
+            tbcIngreso.Controls.Add(mskPrecio);
+            tbcIngreso.Controls.Add(cmbNumero);
+            tbcIngreso.Controls.Add(cmbMarca);
+            tbcIngreso.Controls.Add(btnRegistrar);
+            tbcIngreso.Controls.Add(cmbOrigen);
+            tbcIngreso.Controls.Add(lblPrecio);
+            tbcIngreso.Controls.Add(lblMarca);
+            tbcIngreso.Controls.Add(lblOrigen);
+            tbcIngreso.Controls.Add(lblNumero);
+            tbcIngreso.Controls.Add(lblDescripcion);
+            tbcIngreso.Location = new Point(4, 24);
+            tbcIngreso.Name = "tbcIngreso";
+            tbcIngreso.Padding = new Padding(3);
+            tbcIngreso.Size = new Size(470, 355);
+            tbcIngreso.TabIndex = 0;
+            tbcIngreso.Text = "Ingreso";
+            tbcIngreso.UseVisualStyleBackColor = true;
+            // 
+            // ltbDescripcion
+            // 
+            ltbDescripcion.FormattingEnabled = true;
+            ltbDescripcion.ItemHeight = 15;
+            ltbDescripcion.Location = new Point(6, 125);
+            ltbDescripcion.Name = "ltbDescripcion";
+            ltbDescripcion.Size = new Size(443, 154);
+            ltbDescripcion.TabIndex = 14;
+            // 
+            // mskPrecio
+            // 
+            mskPrecio.Location = new Point(282, 55);
+            mskPrecio.Mask = "$9999999999";
+            mskPrecio.Name = "mskPrecio";
+            mskPrecio.Size = new Size(167, 23);
+            mskPrecio.TabIndex = 13;
+            mskPrecio.ValidatingType = typeof(int);
+            // 
+            // cmbNumero
+            // 
+            cmbNumero.FormattingEnabled = true;
+            cmbNumero.Location = new Point(73, 52);
+            cmbNumero.Name = "cmbNumero";
+            cmbNumero.Size = new Size(141, 23);
+            cmbNumero.TabIndex = 12;
+            // 
+            // cmbMarca
+            // 
+            cmbMarca.FormattingEnabled = true;
+            cmbMarca.Items.AddRange(new object[] { "(P) Peugeot", "(F) Fiat", "(R) Renault" });
+            cmbMarca.Location = new Point(73, 13);
+            cmbMarca.Name = "cmbMarca";
+            cmbMarca.Size = new Size(141, 23);
+            cmbMarca.TabIndex = 11;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(340, 310);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(109, 23);
+            btnRegistrar.TabIndex = 10;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // cmbOrigen
+            // 
+            cmbOrigen.FormattingEnabled = true;
+            cmbOrigen.Items.AddRange(new object[] { "(I) Importado", "(N) Nacional" });
+            cmbOrigen.Location = new Point(282, 13);
+            cmbOrigen.Name = "cmbOrigen";
+            cmbOrigen.Size = new Size(167, 23);
+            cmbOrigen.TabIndex = 9;
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(230, 55);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(43, 15);
+            lblPrecio.TabIndex = 6;
+            lblPrecio.Text = "precio:";
+            // 
+            // tbcDatos
+            // 
+            tbcDatos.Controls.Add(lblOrigen2);
+            tbcDatos.Controls.Add(lblMarca2);
+            tbcDatos.Controls.Add(cmbOrigenFiltro);
+            tbcDatos.Controls.Add(cboMarcaFiltro);
+            tbcDatos.Controls.Add(dgvDatos);
+            tbcDatos.Location = new Point(4, 24);
+            tbcDatos.Name = "tbcDatos";
+            tbcDatos.Padding = new Padding(3);
+            tbcDatos.Size = new Size(470, 355);
+            tbcDatos.TabIndex = 1;
+            tbcDatos.Text = "Datos";
+            tbcDatos.UseVisualStyleBackColor = true;
             // 
             // lblOrigen2
             // 
             lblOrigen2.AutoSize = true;
-            lblOrigen2.Location = new Point(22, 314);
+            lblOrigen2.Location = new Point(236, 53);
             lblOrigen2.Name = "lblOrigen2";
-            lblOrigen2.Size = new Size(46, 15);
-            lblOrigen2.TabIndex = 8;
-            lblOrigen2.Text = "Origen:";
+            lblOrigen2.Size = new Size(43, 15);
+            lblOrigen2.TabIndex = 4;
+            lblOrigen2.Text = "Origen";
             // 
-            // rdbPeugeot
+            // lblMarca2
             // 
-            rdbPeugeot.AutoSize = true;
-            rdbPeugeot.Location = new Point(112, 35);
-            rdbPeugeot.Name = "rdbPeugeot";
-            rdbPeugeot.Size = new Size(69, 19);
-            rdbPeugeot.TabIndex = 9;
-            rdbPeugeot.TabStop = true;
-            rdbPeugeot.Text = "Peugeot";
-            rdbPeugeot.UseVisualStyleBackColor = true;
+            lblMarca2.AutoSize = true;
+            lblMarca2.Location = new Point(17, 53);
+            lblMarca2.Name = "lblMarca2";
+            lblMarca2.Size = new Size(40, 15);
+            lblMarca2.TabIndex = 3;
+            lblMarca2.Text = "Marca";
             // 
-            // rdbFiat
+            // cmbOrigenFiltro
             // 
-            rdbFiat.AutoSize = true;
-            rdbFiat.Location = new Point(206, 35);
-            rdbFiat.Name = "rdbFiat";
-            rdbFiat.Size = new Size(44, 19);
-            rdbFiat.TabIndex = 10;
-            rdbFiat.TabStop = true;
-            rdbFiat.Text = "Fiat";
-            rdbFiat.UseVisualStyleBackColor = true;
+            cmbOrigenFiltro.FormattingEnabled = true;
+            cmbOrigenFiltro.Location = new Point(285, 45);
+            cmbOrigenFiltro.Name = "cmbOrigenFiltro";
+            cmbOrigenFiltro.Size = new Size(121, 23);
+            cmbOrigenFiltro.TabIndex = 2;
             // 
-            // rdbRenault
+            // cboMarcaFiltro
             // 
-            rdbRenault.AutoSize = true;
-            rdbRenault.Location = new Point(274, 35);
-            rdbRenault.Name = "rdbRenault";
-            rdbRenault.Size = new Size(65, 19);
-            rdbRenault.TabIndex = 11;
-            rdbRenault.TabStop = true;
-            rdbRenault.Text = "Renault";
-            rdbRenault.UseVisualStyleBackColor = true;
+            cboMarcaFiltro.FormattingEnabled = true;
+            cboMarcaFiltro.Location = new Point(63, 45);
+            cboMarcaFiltro.Name = "cboMarcaFiltro";
+            cboMarcaFiltro.Size = new Size(121, 23);
+            cboMarcaFiltro.TabIndex = 1;
             // 
-            // txtNumero
+            // dgvDatos
             // 
-            txtNumero.Location = new Point(108, 113);
-            txtNumero.Name = "txtNumero";
-            txtNumero.Size = new Size(231, 23);
-            txtNumero.TabIndex = 13;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Columns.AddRange(new DataGridViewColumn[] { colMarca, colOrigen, colNumero, colPrecio, colDescripcion });
+            dgvDatos.Location = new Point(6, 83);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.Size = new Size(446, 252);
+            dgvDatos.TabIndex = 0;
+            dgvDatos.CellContentClick += dataGridView1_CellContentClick;
             // 
-            // txtDescripcion
+            // colMarca
             // 
-            txtDescripcion.Location = new Point(108, 152);
-            txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(231, 23);
-            txtDescripcion.TabIndex = 14;
+            colMarca.HeaderText = "Marca";
+            colMarca.Name = "colMarca";
             // 
-            // btnIngresarRepuestos
+            // colOrigen
             // 
-            btnIngresarRepuestos.Location = new Point(139, 202);
-            btnIngresarRepuestos.Name = "btnIngresarRepuestos";
-            btnIngresarRepuestos.Size = new Size(167, 23);
-            btnIngresarRepuestos.TabIndex = 15;
-            btnIngresarRepuestos.Text = "Ingresar Repuestos";
-            btnIngresarRepuestos.UseVisualStyleBackColor = true;
+            colOrigen.HeaderText = "Origen";
+            colOrigen.Name = "colOrigen";
             // 
-            // cmbMarca2
+            // colNumero
             // 
-            cmbMarca2.FormattingEnabled = true;
-            cmbMarca2.Location = new Point(109, 264);
-            cmbMarca2.Name = "cmbMarca2";
-            cmbMarca2.Size = new Size(121, 23);
-            cmbMarca2.TabIndex = 16;
+            colNumero.HeaderText = "Numero";
+            colNumero.Name = "colNumero";
             // 
-            // cmbOrigen2
+            // colPrecio
             // 
-            cmbOrigen2.FormattingEnabled = true;
-            cmbOrigen2.Location = new Point(108, 306);
-            cmbOrigen2.Name = "cmbOrigen2";
-            cmbOrigen2.Size = new Size(121, 23);
-            cmbOrigen2.TabIndex = 17;
+            colPrecio.HeaderText = "Precio";
+            colPrecio.Name = "colPrecio";
             // 
-            // listBox1
+            // colDescripcion
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(22, 348);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(317, 109);
-            listBox1.TabIndex = 20;
-            // 
-            // rdbNacional
-            // 
-            rdbNacional.AutoSize = true;
-            rdbNacional.Location = new Point(112, 80);
-            rdbNacional.Name = "rdbNacional";
-            rdbNacional.Size = new Size(72, 19);
-            rdbNacional.TabIndex = 21;
-            rdbNacional.TabStop = true;
-            rdbNacional.Text = "Nacional";
-            rdbNacional.UseVisualStyleBackColor = true;
-            // 
-            // rdbImportado
-            // 
-            rdbImportado.AutoSize = true;
-            rdbImportado.Location = new Point(206, 80);
-            rdbImportado.Name = "rdbImportado";
-            rdbImportado.Size = new Size(81, 19);
-            rdbImportado.TabIndex = 22;
-            rdbImportado.TabStop = true;
-            rdbImportado.Text = "Importado";
-            rdbImportado.UseVisualStyleBackColor = true;
+            colDescripcion.HeaderText = "Descripcion";
+            colDescripcion.Name = "colDescripcion";
             // 
             // VentaDeRepuestos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(386, 469);
-            Controls.Add(rdbImportado);
-            Controls.Add(rdbNacional);
-            Controls.Add(listBox1);
-            Controls.Add(cmbOrigen2);
-            Controls.Add(cmbMarca2);
-            Controls.Add(btnIngresarRepuestos);
-            Controls.Add(txtDescripcion);
-            Controls.Add(txtNumero);
-            Controls.Add(rdbRenault);
-            Controls.Add(rdbFiat);
-            Controls.Add(rdbPeugeot);
-            Controls.Add(lblOrigen2);
-            Controls.Add(lblmarca2);
-            Controls.Add(lblDescripcion);
-            Controls.Add(lblNumero);
-            Controls.Add(lblOrigen);
-            Controls.Add(lblMarca);
+            ClientSize = new Size(564, 469);
+            Controls.Add(Datos);
             Name = "VentaDeRepuestos";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Venta de Repuestos";
+            Datos.ResumeLayout(false);
+            tbcIngreso.ResumeLayout(false);
+            tbcIngreso.PerformLayout();
+            tbcDatos.ResumeLayout(false);
+            tbcDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -238,18 +295,27 @@
         private Label lblOrigen;
         private Label lblNumero;
         private Label lblDescripcion;
-        private Label lblmarca2;
-        private Label lblOrigen2;
-        private RadioButton rdbPeugeot;
-        private RadioButton rdbFiat;
-        private RadioButton rdbRenault;
-        private TextBox txtNumero;
-        private TextBox txtDescripcion;
         private Button btnIngresarRepuestos;
-        private ComboBox cmbMarca2;
-        private ComboBox cmbOrigen2;
+        private TabControl Datos;
+        private TabPage tbcIngreso;
+        private ComboBox cboMarcaFiltro;
         private ListBox listBox1;
-        private RadioButton rdbNacional;
-        private RadioButton rdbImportado;
+        private Label lblPrecio;
+        private TabPage tbcDatos;
+        private DataGridView dgvDatos;
+        private Button btnRegistrar;
+        private ComboBox cmbOrigen;
+        private DataGridViewTextBoxColumn colMarca;
+        private DataGridViewTextBoxColumn colOrigen;
+        private DataGridViewTextBoxColumn colNumero;
+        private DataGridViewTextBoxColumn colPrecio;
+        private DataGridViewTextBoxColumn colDescripcion;
+        private ListBox ltbDescripcion;
+        private MaskedTextBox mskPrecio;
+        private ComboBox cmbNumero;
+        private ComboBox cmbMarca;
+        private ComboBox cmbOrigenFiltro;
+        private Label lblOrigen2;
+        private Label lblMarca2;
     }
 }
--- pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.cs
+++ pry_Soria_SP2_VentaDeRepuestos/VentaDeRepuestos.cs
@@ -6,5 +6,53 @@ namespace pry_Soria_SP2_VentaDeRepuestos
         {
             InitializeComponent();
         }
+
+        string[,] matRespuesto = new string[4, 5];
+
+        int indiceGrabar = 0;
+
+ private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            
+                matRespuesto[indiceGrabar, 0] = "marca";
+                matRespuesto[indiceGrabar, 1] = "origen";
+                matRespuesto[indiceGrabar, 2] = "numero";
+                matRespuesto[indiceGrabar, 3] = "precio";
+                matRespuesto[indiceGrabar, 4] = "descripcion";
+
+                indiceGrabar++;
+
+            
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            List<string> origenes = new List<string>();
+            List<string> listaMarcas = new List<string>();
+
+            for (int filas = 0; filas < matRespuesto.GetLength(0); filas++)
+            {
+                dgvDatos.Rows.Add(matRespuesto[filas, 0],
+                    matRespuesto[filas, 1],
+                    matRespuesto[filas, 2],
+                    matRespuesto[filas, 3],
+                    matRespuesto[filas, 4]);
+
+                if (!listaMarcas.Contains(matRespuesto[filas, 0]))
+                {
+                    listaMarcas.Add(matRespuesto[filas, 0]);
+                }
+
+                if (!origenes.Contains(matRespuesto[filas, 1]))
+                {
+                    origenes.Add(matRespuesto[filas, 1]);
+                }
+            }
+
+            cboMarcaFiltro.DataSource = listaMarcas;
+            cmbOrigenFiltro.DataSource = origenes;
+
+        }
+
+       
     }
 }
